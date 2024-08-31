205de518fad9d5449788e7b2a0926bd0c96e686e
senice222 <sasdasd920@gmail.com>
2024-08-31 16:19:17 +0300

    refactor

--- shared/components/button.tsx
+++ shared/components/button.tsx
@@ -1,32 +1,26 @@
-'use client';
-import React, {Suspense} from 'react';
-import {useRouter} from 'next/navigation';
+"use client";
+import React from "react";
+import Link from "next/link";
 
 interface ButtonProps {
-    text: string;
-    disabled: boolean;
-    href: string;
+  text: string;
+  disabled: boolean;
+  href: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({text, disabled, href}) => {
-    const router = useRouter();
+export const Button: React.FC<ButtonProps> = ({ text, disabled, href }) => {
 
-    const handleClick = () => {
-        if (!disabled) {
-            router.push(href);
-        }
-    };
-
-    return (
-        <button
-            onClick={handleClick}
-            className={`w-full py-2 px-4 rounded-md text-white text-center transition-all duration-200 ease-in-out
-            ${disabled
-                ? 'bg-gray-500 cursor-not-allowed opacity-50 border-gray-400'
-                : 'bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700'}`}
-            disabled={disabled}
-        >
-            {text}
-        </button>
-    );
+  return (
+    <Link href={disabled ? "#" : href}>
+      <button
+        className={`w-full py-2 px-4 rounded-md text-white text-center transition-all duration-200 ease-in-out
+        ${disabled
+          ? "bg-gray-500 cursor-not-allowed opacity-50 border-gray-400"
+          : "bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700"}`}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    </Link>
+  );
 };

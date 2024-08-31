82deda225c2f3c2ecfc0c07fcd2cbfa76f13d7af
senice222 <sasdasd920@gmail.com>
2024-08-31 13:30:34 +0300

    refactor

--- app/result/[makeId]/[year]/page.tsx
+++ app/result/[makeId]/[year]/page.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { FilteredVehicles, VehicleModel } from "@/shared/types/Vehicles";
-import '../../../../envConfig'
-import { fetchVehicleModels } from "@/shared/services/get-vehicle-models";
+import React from "react";
+import { Result, Vehicle } from "@/shared/types/Vehicles";
+import "../../../../envConfig";
+import { fetchVehicleModels, getAllVehicles } from "@/shared/services/get-vehicle-models";
 import ResultList from "@/shared/components/result/result-list";
 
 // I was thinking about adding suspense anywhere, but didn't find some case where i can use it. Everywhere using SSR
 
 export async function generateStaticParams() {
-    const vehicles: FilteredVehicles = await fetch(`${process.env.API_URL}/vehicles/GetMakesForVehicleType/car?format=json`).then((res) => res.json());
+  const data: Vehicle = await getAllVehicles();
 
-    return vehicles.Results.map((vehicle: VehicleModel) => ({
-        id: vehicle.Make_ID,
-    }));
+  return data.Results.map((vehicle: Result) => ({
+    id: vehicle.MakeId
+  }));
 }
 
-const ResultPage: React.FC<{ params: { makeId: string; year: string } }> = async ({params}) => {
-    const {makeId, year} = params;
-    const result = await fetchVehicleModels(makeId, year);
+const ResultPage: React.FC<{ params: { makeId: string; year: string } }> = async ({ params }) => {
+  const { makeId, year } = params;
+  const result = await fetchVehicleModels(makeId, year);
 
-    return <ResultList result={result.Results}/>
+  return <ResultList result={result.Results} />;
 };
 
 export default ResultPage;
